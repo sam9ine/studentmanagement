756847995b258ee3f10ccd44810a5d472ad92a0e
sameer <sameerr1105@gmail.com>
2026-05-05 11:54:40 +0530

    Added DBconnection

---
+++ src/com/project/dao/StudentDao.java
@@ -0,0 +1,58 @@
+package com.project.dao;
+
+import com.project.db.DBconnection;
+import java.sql.*;
+
+public class StudentDao {
+
+    public String addStudent(String name,String course,String email) throws Exception {
+
+        String Query= "INSERT INTO students(name,course,email) VALUES(?,?,?)";
+
+        try(Connection connect =DBconnection.getConnection();
+            PreparedStatement ps = connect.prepareStatement(Query)){
+
+            ps.setString(1,name);
+            ps.setString(2,course);
+            ps.setString(3,email);
+
+            int rows=ps.executeUpdate();
+
+            if(rows>0){
+                return "Success";
+            }
+            else{
+                return "Error";
+            }
+        }
+        catch(Exception e){
+            return "Exception: "+ e.getMessage();
+        }
+    }
+
+
+    public void getAllStudents() {
+        String query = "SELECT * FROM students";
+
+        try (Connection con = DBconnection.getConnection();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
+
+            System.out.println("Student List");
+            while (rs.next()) {
+
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String course = rs.getString("course");
+                String email = rs.getString("email");
+
+                System.out.println(id + " | " + name + " | " + course + " | " + email);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/com/project/db/DBconnection.java
@@ -0,0 +1,14 @@
+package com.project.db;
+
+import java.sql.*;
+
+public class DBconnection {
+    public static Connection getConnection()throws Exception{
+        String url="jdbc:mysql://localhost:3306/studentdb";
+        String user ="root";
+        String password ="";
+
+        return DriverManager.getConnection(url,user,password);
+    }
+
+}
