0161d61346349e196fb2b65e73fafdfd7b8d64b1
sameer <sameerr1105@gmail.com>
2026-05-05 12:28:20 +0530

    added clienthandler

--- src/com/project/server/ClientHandler.java
+++ src/com/project/server/ClientHandler.java
@@ -1,5 +1,39 @@
 package com.project.server;
 
-public class ClientHandler {
+import com.project.dao.StudentDao;
+import com.project.model.Student;
+import java.io.*;
+import java.net.*;
 
+public class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private StudentDao studentDao;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+        this.studentDao = new StudentDao();
+    }
+
+    @Override
+    public void run() {
+        try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+            Student student = (Student) in.readObject();
+            System.out.println("Server received data for: " + student.getName());
+
+            String result = studentDao.addStudent(student);
+
+            out.println(result);
+
+        } catch (Exception e) {
+            System.out.println("ClientHandler Error: " + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
